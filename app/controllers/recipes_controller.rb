223b5624a1ec0d00a844d135a79c72d1296c5a7e
domanhtien2011 <domanhtien2011@gmail.com>
2016-01-18 20:15:00 +0700

    Add form and partials

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,52 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(1)
+
+    if @recipe.save
+      flash[:success] = "Recipe was created successfully"
+      redirect_to(@recipe)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was updated successfully"
+      redirect_to(@recipe)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
+end
