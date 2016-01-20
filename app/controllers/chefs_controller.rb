645f0f4a980bb0a7da738ea293f008996f6e387e
Tien Do <domanhtien2011@hotmail.com>
2016-01-20 14:29:30 +0700

    Add Chef Show and Index

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,6 +1,47 @@
 class ChefsController < ApplicationController
+  before_action :set_chef, except: [:new, :create, :index]
+
+  def show
+    @recipes = @chef.recipes.paginate(page: params[:page], per_page: 3)
+  end
+
+  def index
+    @chefs = Chef.all
+  end
 
   def new
-
+    @chef = Chef.new
   end
+
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "Your account was successfully created"
+      redirect_to recipes_url
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @chef.update(chef_params)
+      flash[:success] = "Your profile was successfully updated"
+      redirect_to recipes_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def chef_params
+      params.require(:chef).permit(:chefname, :email, :password)
+    end
+
+    def set_chef
+      @chef = Chef.find(params[:id])
+    end
 end
